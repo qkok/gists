3f4ab96d0c402fd0552d5de8598f3a0ac4c5d17c
qkok <quartusk@gmail.com>
2018-09-05 07:58:27 +0200

    Added text chunk gist

---
+++ Python/text_chunks.py
@@ -0,0 +1,43 @@
+def get_chunks(message, length):
+  """Takes a message and breaks it into chunks of a given length.
+  Chunks may not exceed the length and words can't be broken up.
+
+  Args:
+    message (string): The message to break into chnuks
+    length (int): The max length of a chunk
+
+  Returns:
+    A list of chunks
+  """
+
+  # splits the message on spaces
+  exploded_message = message.split()
+  new_message = []
+  count = 0
+
+  for current_message in exploded_message:
+    # a word exceeds the minimum length for a chunk
+    if len(current_message) > length:
+        return -1
+
+    # test whether a chunk already exists to be added on to
+    try:
+      if new_message[count]:
+        record_exists = True
+    except IndexError:
+      record_exists = False
+
+    # creates a new chunk
+    if not record_exists and len(current_message) <= length:
+      new_message.append(current_message)
+    # adds another word to a chunk
+    elif record_exists and len(new_message[count] + ' ' + current_message) <= length:
+      new_message[count] += ' ' + current_message
+    # ends a chunk
+    else:
+      count += 1
+      new_message.append(current_message)
+
+  return new_message
+
+print(get_chunks("Hi, my name is Bob", 7));
