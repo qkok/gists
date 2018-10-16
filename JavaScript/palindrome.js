466d6471bb725097b6062bf0dd8126ea201ca805
qkok <quartusk@gmail.com>
2018-10-16 08:36:31 +0200

    Added palindrome checker

---
+++ JavaScript/palindrome.js
@@ -0,0 +1,28 @@
+// input from terminal
+const readline = require('readline');
+let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Word: ', function (word) {
+  if (isPalindrome(word)) {
+    console.log(`${word} is a palindrome`);
+  } else {
+    console.log(`${word} is not a palindrome`);
+  }
+
+  rl.close();
+  process.stdin.destroy();
+});
+
+/**
+ * Checks whether a word is a palindrome
+ * 
+ * @param  {string} word The word to test
+ * @return {boolean} Whether or not the word is a palindrome
+ */
+function isPalindrome(word) {
+  return word.toUpperCase() == word.split('').reverse().join('').toUpperCase();
+}
+
